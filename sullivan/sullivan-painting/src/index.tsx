888665234b5859b818f5683ef58968ab5929b992
Patrick Metsch <patrickmetsch@patricks-mbp.attlocal.net>
2019-11-20 00:26:08 -0500

    initial upload

---
+++ sullivan/sullivan-painting/src/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './App';
+
+const renderApp = (state:any) => {
+
+  const updateGreetingText = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target.value;
+    const greetingText = target === '' ? initialState.greetingText : target;
+    renderApp({
+      ...state,
+      greetingText
+    });
+  }
+
+  const siteProps = {
+    ...state,
+    updateGreetingText
+  }
+
+  ReactDOM.render(
+    <App {...siteProps} />,
+    document.getElementById('root')
+  );
+}
+
+const initialState = {
+  greetingText: "Hello World!"
+}
+
+renderApp(initialState);
